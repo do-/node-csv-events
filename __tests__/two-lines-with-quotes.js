166cedfe13896173db57bb36315476659284365e
Dmitry Ovsyanko <do_@pochta.ru>
2023-04-22 22:12:36 +0300

    line breaks

---
+++ __tests__/two-lines-with-quotes.js
@@ -0,0 +1,23 @@
+const {CSVEventEmitter} = require ('..')
+
+test ('two lines no quotes', () => {
+
+	const c = new CSVEventEmitter ({})
+	
+	const a = []; 
+	
+	c.on ('r', s => a.push ([]))
+	c.on ('c', s => a [a.length - 1].push (s))
+	
+	c.write ('1,2')
+	c.write ('2,3\r\n')
+
+	c.end ('4,"5,5\n5,5",6\r\n')
+
+	expect (a).toStrictEqual ([
+		['1', '22', '3'],
+		['4', '"5,5\n5,5"', '6'],
+	])
+
+})
+
