b5d1162adb9fac3e5d1a0ec8f668b86ba4999ce8
Dmitry Ovsyanko <do_@pochta.ru>
2023-04-23 09:53:00 +0300

    code cleanup, fixes

--- __tests__/two-lines-with-quotes.js
+++ __tests__/two-lines-with-quotes.js
@@ -9,14 +9,14 @@ test ('two lines no quotes', () => {
 	c.on ('r', s => a.push ([]))
 	c.on ('c', s => a [a.length - 1].push (s))
 	
-	c.write ('1,2')
+	c.write ('"",2')
 	c.write ('2,3\r\n')
 
-	c.end ('4,"5,5\n5,5",6\r\n')
+	c.end ('4,"5,5\n""5"",5",6\r\n')
 
 	expect (a).toStrictEqual ([
-		['1', '22', '3'],
-		['4', '"5,5\n5,5"', '6'],
+		['""', '22', '3'],
+		['4', '"5,5\n""5"",5"', '6'],
 	])
 
 })
