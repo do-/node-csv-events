83607c0f41e85b448df55c1f099c1f7525f498f2
Dmitry Ovsyanko <do_@pochta.ru>
2023-04-23 11:58:41 +0300

    +.column property

--- __tests__/two-lines-no-quotes.js
+++ __tests__/two-lines-no-quotes.js
@@ -4,20 +4,17 @@ test ('two lines no quotes', () => {
 
 	const c = new CSVEventEmitter ({delimiter: ';'})
 	
-	let all = [], cur = [] 
+	let all = []
 	
-	c.on ('r', () => {all.push (cur); cur = []})
-	c.on ('c', () => cur.push (c.value))
-	
+	c.on ('c', () => {
+		if (c.column === 1) all.push (c.value)
+	})
+
 	c.write ('1;21,')
 	c.write ('22,23;3\r\n')
 
 	c.end ('4;5;6\r\n')
 
-	expect (all).toStrictEqual ([
-		['1', '21,22,23', '3'],
-		['4', '5', '6'],
-	])
+	expect (all).toStrictEqual (['21,22,23', '5'])
 
 })
-
