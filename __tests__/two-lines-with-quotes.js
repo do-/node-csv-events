e7f05a079d41162974cef5dc8446682837c3eab0
Dmitry Ovsyanko <do_@pochta.ru>
2023-04-23 11:33:54 +0300

    unquote -> get value

--- __tests__/two-lines-with-quotes.js
+++ __tests__/two-lines-with-quotes.js
@@ -2,21 +2,23 @@ const {CSVEventEmitter} = require ('..')
 
 test ('two lines no quotes', () => {
 
-	const c = new CSVEventEmitter ({})
+	const c = new CSVEventEmitter ({empty: null})
 	
 	let all = [], cur = [] 
 
-	c.on ('r', s => {all.push (cur); cur = []})
-	c.on ('c', s => cur.push (s))
+	c.on ('r', () => {all.push (cur); cur = []})
+	c.on ('c', s => {
+		cur.push (c.value)
+	})
 	
 	c.write ('"",2')
-	c.write ('2,3\r\n')
+	c.write ('2,"""..."\r\n')
 
-	c.end ('4,"5,5\n""5"",5",6\r\n')
+	c.end ('4,"5\r,5\n""5"",5\r\n""",6\r\n')
 
 	expect (all).toStrictEqual ([
-		['""', '22', '3'],
-		['4', '"5,5\n""5"",5"', '6'],
+		[null, '22', '"...'],
+		['4', '5\r,5\n"5",5\r\n"', '6'],
 	])
 
 })
