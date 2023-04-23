8333d3165e9c59e660affbf74075a24122229e44
Dmitry Ovsyanko <do_@pochta.ru>
2023-04-23 10:05:20 +0300

    the 'r' event: on line end, not start

--- __tests__/two-lines-no-quotes.js
+++ __tests__/two-lines-no-quotes.js
@@ -4,17 +4,17 @@ test ('two lines no quotes', () => {
 
 	const c = new CSVEventEmitter ({})
 	
-	const a = []; 
+	let all = [], cur = [] 
 	
-	c.on ('r', s => a.push ([]))
-	c.on ('c', s => a [a.length - 1].push (s))
+	c.on ('r', s => {all.push (cur); cur = []})
+	c.on ('c', s => cur.push (s))
 	
 	c.write ('1,2')
 	c.write ('2,3\r\n')
 
 	c.end ('4,5,6\r\n')
 
-	expect (a).toStrictEqual ([
+	expect (all).toStrictEqual ([
 		['1', '22', '3'],
 		['4', '5', '6'],
 	])
--- __tests__/two-lines-with-quotes.js
+++ __tests__/two-lines-with-quotes.js
@@ -4,17 +4,17 @@ test ('two lines no quotes', () => {
 
 	const c = new CSVEventEmitter ({})
 	
-	const a = []; 
-	
-	c.on ('r', s => a.push ([]))
-	c.on ('c', s => a [a.length - 1].push (s))
+	let all = [], cur = [] 
+
+	c.on ('r', s => {all.push (cur); cur = []})
+	c.on ('c', s => cur.push (s))
 	
 	c.write ('"",2')
 	c.write ('2,3\r\n')
 
 	c.end ('4,"5,5\n""5"",5",6\r\n')
 
-	expect (a).toStrictEqual ([
+	expect (all).toStrictEqual ([
 		['""', '22', '3'],
 		['4', '"5,5\n""5"",5"', '6'],
 	])
