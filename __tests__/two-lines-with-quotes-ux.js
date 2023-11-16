f6f08c060a72f1698d992c9f07d844ea2f1a2f9e
Dmitry Ovsyanko <do_@pochta.ru>
2023-11-16 19:49:50 +0300

    cr/lf FIX

---
+++ __tests__/two-lines-with-quotes-ux.js
@@ -0,0 +1,51 @@
+const {CSVEventEmitter} = require ('..')
+
+test ('two lines no quotes', async () =>  {
+
+	const all = [], raw = []
+
+	await new Promise ((ok, fail) => {
+
+		const c = new CSVEventEmitter ({empty: null})
+	
+		let cur = [], cr = []
+
+		c.once ('end', ok)
+	
+		c.on ('r', () => {
+			all.push (cur); cur = []
+			raw.push (cr);  cr  = []
+		})
+
+		c.on ('c', s => {
+			cur.push (c.value)
+			cr.push (c.raw)
+		})
+
+		try {
+
+			c.write ('"",2')
+			c.write ('2,"""..."\n')
+		
+			c.end ('4,"5\r,5\n""5"",5\r\n""",6\n')	
+	
+		}
+		catch (err) {
+
+			fail (err)
+
+		}
+
+	}) 
+
+	expect (all).toStrictEqual ([
+		[null, '22', '"...'],
+		['4', '5\r,5\n"5",5\r\n"', '6'],
+	])
+
+	expect (raw).toStrictEqual ([
+		[ '""', '22', '"""..."' ], 
+		[ '4', '"5\r,5\n""5"",5\r\n"""', '6' ] 
+	])
+
+})
