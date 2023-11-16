faeff0e4ae88b9cff93bc6b52a6e6a9495c5af95
Dmitry Ovsyanko <do_@pochta.ru>
2023-11-16 14:23:56 +0300

    +'end' event

--- __tests__/two-lines-with-quotes.js
+++ __tests__/two-lines-with-quotes.js
@@ -1,20 +1,38 @@
 const {CSVEventEmitter} = require ('..')
 
-test ('two lines no quotes', () => {
+test ('two lines no quotes', async () =>  {
 
-	const c = new CSVEventEmitter ({empty: null})
+	const all = []
+
+	await new Promise ((ok, fail) => {
+
+		const c = new CSVEventEmitter ({empty: null})
 	
-	let all = [], cur = [] 
+		let cur = []
 
-	c.on ('r', () => {all.push (cur); cur = []})
-	c.on ('c', s => {
-		cur.push (c.value)
-	})
+		c.once ('end', ok)
 	
-	c.write ('"",2')
-	c.write ('2,"""..."\r\n')
+		c.on ('r', () => {all.push (cur); cur = []})
 
-	c.end ('4,"5\r,5\n""5"",5\r\n""",6\r\n')
+		c.on ('c', s => {
+			cur.push (c.value)
+		})
+
+		try {
+
+			c.write ('"",2')
+			c.write ('2,"""..."\r\n')
+		
+			c.end ('4,"5\r,5\n""5"",5\r\n""",6\r\n')	
+	
+		}
+		catch (err) {
+
+			fail (err)
+
+		}
+
+	}) 
 
 	expect (all).toStrictEqual ([
 		[null, '22', '"...'],
