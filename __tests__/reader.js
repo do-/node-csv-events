e087b8440c060780c9e0468353fbd83f86569582
Dmitry Ovsyanko <do_@pochta.ru>
2023-11-18 09:01:16 +0300

    CSVReader +skip

--- __tests__/reader.js
+++ __tests__/reader.js
@@ -33,6 +33,41 @@ test ('basic', async () =>  {
 
 })
 
+test ('skip header', async () =>  {
+
+	const reader = new CSVReader ({
+		skip: 1,
+		columns: [
+			'id',
+			null, 
+			{name: 'label', raw: true},
+		]
+	})
+
+	expect (reader).toBeInstanceOf (CSVReader)
+
+	const a = []
+
+	await new Promise ((ok, fail) => {
+
+		reader.on ('error', fail)
+		reader.on ('end', ok)
+		reader.on ('data', r => a.push (r))
+
+		reader.write (Buffer.from ('id,is_active,name\n', 'utf-8'))
+		reader.write (Buffer.from ('"1",true,One\n', 'utf-8'))
+		reader.end   (Buffer.from ('2,false,"Two"\n', 'utf-8'))
+
+	})
+
+	expect (a).toStrictEqual ([
+		{id: '1', label: 'One'}, 
+		{id: '2', label: '"Two"'},
+	])
+
+})
+
+
 test ('bigint', () => {
 
 	expect (new CSVReader ({columns: '_ '.repeat (32).trim ().split (' ')}).csv.unit).toBe (1)
@@ -46,6 +81,8 @@ test ('bad options', () => {
 	expect (() => new CSVReader ()).toThrow ()
 	expect (() => new CSVReader ({})).toThrow ()
 	expect (() => new CSVReader ({columns: {}})).toThrow ()
+	expect (() => new CSVReader ({columns: [], skip: null})).toThrow ()
+	expect (() => new CSVReader ({columns: [], skip: -1})).toThrow ()
 
 })
 
