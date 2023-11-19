368ac80e54173ac7c2ca9af650ce2bdbfd0fd110
Dmitry Ovsyanko <do_@pochta.ru>
2023-11-19 08:51:49 +0300

    CSVEventEmitter FIX (last line may not be ended)

--- __tests__/one-line-no-quotes.js
+++ __tests__/one-line-no-quotes.js
@@ -11,13 +11,23 @@ test ('edge cases', () => {
 
 test ('one line no quotes', () => {
 
-	const c = new CSVEventEmitter ({mask: 0xF})
-	
-	const a = []; c.on ('c', () => a.push (c.value))
-	
-	c.end ('1,,3')
+	const RESULT = ['1', '', 'supervisor']
 
-	expect (a).toStrictEqual (['1', '', '3'])
+	const p = s => {
+
+		const c = new CSVEventEmitter ({mask: 0xF})
+	
+		const a = []; c.on ('c', () => a.push (c.value))
+		
+		c.end (s)
+
+		return a
+	
+	}
+	
+	expect (p ('1,,supervisor')).toStrictEqual (RESULT)
+	expect (p ('1,,supervisor\n')).toStrictEqual (RESULT)
+	expect (p ('1,,supervisor\r\n')).toStrictEqual (RESULT)
 
 })
 
