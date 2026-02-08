a39b63dedc7aac0c3fe8337a4455e6c83830a4fa
Dmitry Ovsyanko <dovsyanko@cheby.ru>
2026-02-08 14:39:29 +0300

    CSVEventEmitter: 0 mask means "all"

--- __tests__/two-lines-no-quotes.js
+++ __tests__/two-lines-no-quotes.js
@@ -2,7 +2,7 @@ const {CSVEventEmitter} = require ('..')
 
 test ('two lines no quotes', () => {
 
-	const c = new CSVEventEmitter ({delimiter: ';', mask: 0xF})
+	const c = new CSVEventEmitter ({delimiter: ';'})
 	
 	let all = []
 	
